8a9d34176fe88043ad5ac10d75039ae94779f46a
Nibalab <abdalbakinibal@gmail.com>
2024-07-07 18:50:54 +0300

    updating client.py

--- client.py
+++ client.py
@@ -1,6 +1,19 @@
+import yaml
+import os
+
 class Client:
-    def __init__(self, ip_address):
-        self.ip_address = ip_address
+    def __init__(self):
+        config = self.load_config()
+        self.ip_address = config['ServerIPAddress']
+
+    def load_config(self):
+        config_path = os.path.join(os.path.dirname(__file__), '../config.yaml')
+        with open(config_path, 'r') as file:
+            return yaml.safe_load(file)
 
     def print_ip(self):
         print(f"Client IP Address: {self.ip_address}")
+
+if __name__ == "__main__":
+    client = Client()
+    client.print_ip()
